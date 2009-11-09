a38420f5293e7fc2917c6e2fbd35081790f0c5dd
falti <faltibrain@gmail.com>
2009-11-09 23:22:08 +0100

    added second test for xml format

--- test/functional/milestones_controller_test.rb
+++ test/functional/milestones_controller_test.rb
@@ -31,14 +31,29 @@ class MilestonesControllerTest < ActionController::TestCase
       news.expects(:created_on).returns(Time.now)
       news.expects(:description).returns("This is a description")
       
-      
+      @request.session[:user_id] = 2
+      get :index, :format => "html" 
+      assert_response :success
+      assert_template 'index'
+    end
+    
+    def test_index_xml
+      project = Project.new
+      version = Version.new
+      news_collection = []
+      news = News.new
+      Project.expects(:find).returns([project]).at_least_once
+      project.expects(:versions).returns([version]).at_least_once
+      project.expects(:news).returns(news_collection).at_least_once
+      news_collection.expects(:first).returns(news).at_least_once
+      news.expects(:summary).returns("This is a summary")
+      news.expects(:description).returns("This is a description")
       
       @request.session[:user_id] = 2
-      get :index
+      get :index, :format => "xml" 
       assert_response :success
       assert_template 'index'
       
-    
     end
   
   else
